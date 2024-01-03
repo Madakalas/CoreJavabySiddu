9473a33f835466835bc5c878094baad4f2f0b7bf
Siddu Reddy <siddureddy@Siddu-Reddys-MacBook-Air.local>
2024-01-03 12:33:18 +0530

    test

--- Collection FW/One.java
+++
@@ -1,8 +0,0 @@
-class One{
-    public static void main(String[] args) {
-        int[] ids={101,102,103,104};
-        System.out.println(ids);
-        //we cant increase/decrease the size -fixed
-        // hold only homogeneous data
-    }
-}
---
+++ CollectionFW/Three.java
@@ -0,0 +1,8 @@
+public class Three {
+    public static void main(String[] args) {
+        //creation of array using class name with size
+        Three[] x = new Three[5];
+        
+    }
+    
+}
