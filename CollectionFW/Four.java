eddbc8272488dc903eb5554603e6fea51170d68d
Siddu Reddy <siddureddy@Siddu-Reddys-MacBook-Air.local>
2024-01-04 21:56:48 +0530

    test

---
+++ CollectionFW/Four.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+//we need import arraylist to accesss in built methods
+public class Four {
+    public static void main(String[] args) {
+        ArrayList AL = new ArrayList<>();
+        AL.add(101);
+        AL.add(102);
+        AL.add(103);
+        AL.add(104);
+
+        
+    }
+    
+}
